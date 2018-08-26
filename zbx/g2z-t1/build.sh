6ca0e061e08a92e436050ab3075a63866d7da29b
Markus Fischbacher <fischbacher.markus@gmail.com>
2018-08-26 14:00:43 +0200

    whatever

--- zbx/g2z-t1/build.sh
+++ zbx/g2z-t1/build.sh
@@ -1,1 +1,2 @@
+docker run --rm -ti -e CGO_LDFLAGS_ALLOW=".*\.*" -v "$PWD":/go/src -w /go/src golang:1.10 make clean
 docker run --rm -ti -e CGO_LDFLAGS_ALLOW=".*\.*" -v "$PWD":/go/src -w /go/src golang:1.10 make
